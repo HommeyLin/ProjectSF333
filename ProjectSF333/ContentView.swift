6bd12b7ced86bbd8bcba48b4c82401eb166ff2e6
HommeyLin <saruttaya.att@gmail.com>
2023-12-30 16:42:11 +0700

    Initial Commit

---
+++ ProjectSF333/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectSF333
+//
+//  Created by saruttaya attramongkol on 30/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectSF333/ProjectSF333App.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSF333App.swift
+//  ProjectSF333
+//
+//  Created by saruttaya attramongkol on 30/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSF333App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
